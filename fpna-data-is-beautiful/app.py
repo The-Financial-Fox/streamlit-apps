55eaf418d01d016d72d363dd1cf94811262fe86c
The-Financial-Fox <46638505+The-Financial-Fox@users.noreply.github.com>
2025-01-08 05:54:13 +0100

    Update app.py

--- fpna-data-is-beautiful/app.py
+++ fpna-data-is-beautiful/app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
+import numpy as np
 
 # Title and Introduction
 st.title("FP&A Data is Beautiful")
