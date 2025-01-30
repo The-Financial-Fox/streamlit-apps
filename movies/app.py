202fd25e65a529f41631d1cb1f701be03f01537f
The-Financial-Fox <46638505+The-Financial-Fox@users.noreply.github.com>
2025-01-30 20:08:32 +0100

    Update app.py

--- movies/app.py
+++ movies/app.py
@@ -5,6 +5,8 @@ import pandas as pd
 def load_data():
     file_url = "https://raw.githubusercontent.com/The-Financial-Fox/streamlit-apps/refs/heads/main/movies/Top_1000_IMDb_movies_New_version.csv"
     df = pd.read_csv(file_url)
+    st.write("### First few rows of dataset:")
+    st.write(df.head())  # Debugging: Show the first few rows
     st.write("### Dataset Columns:", df.columns.tolist())  # Debugging: Show column names
     return df
 
